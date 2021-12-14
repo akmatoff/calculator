b4603b3c6d4d65981762ae6a0b7bb344318dc4b6
akmatoff <akmatoff.exe@gmail.com>
2021-12-14 15:20:40 +0600

    initial commit

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,73 @@
+import "./Calculator.css";
+
+function Calculator() {
+  return (
+    <div id="calculator" className="flex-column">
+      <div id="display" className="flex-row">
+        0
+      </div>
+      <div id="buttons-container">
+        <div id="clear" className="button">
+          AC
+        </div>
+        <div id="divide" className="button">
+          /
+        </div>
+        <div id="multiple" className="button">
+          X
+        </div>
+
+        <div id="one" className="button">
+          1
+        </div>
+        <div id="two" className="button">
+          2
+        </div>
+        <div id="three" className="button">
+          3
+        </div>
+
+        <div id="subtract" className="button">
+          -
+        </div>
+
+        <div id="four" className="button">
+          4
+        </div>
+        <div id="five" className="button">
+          5
+        </div>
+        <div id="six" className="button">
+          6
+        </div>
+
+        <div id="add" className="button">
+          +
+        </div>
+
+        <div id="seven" className="button">
+          7
+        </div>
+        <div id="eight" className="button">
+          8
+        </div>
+        <div id="nine" className="button">
+          9
+        </div>
+
+        <div id="equals" className="button">
+          =
+        </div>
+
+        <div id="zero" className="button">
+          0
+        </div>
+        <div id="decimal" className="button">
+          .
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
